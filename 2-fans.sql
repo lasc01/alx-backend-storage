66e2bccb3ef628c45193771ffc18a774c5bad6d1
Abioro Olamide <olamideabioro@gmail.com>
2023-07-12 13:40:06 +0100

    Delete 2-fans.sql

--- 2-fans.sql
+++
@@ -1,5 +0,0 @@
--- Task 2: 2. Best band ever! - ranks country origins of bands,
--- ordered by the number of (non-unique) fans
-SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
-GROUP BY `origin`
-ORDER BY `nb_fans` DESC;
