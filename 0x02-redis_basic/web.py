40908453637a876ef7239fa1d308060812eab185
lasc01 <olamideabioro@gmail.com>
2023-07-19 14:32:07 +0000

    update on web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,39 +1,21 @@
 #!/usr/bin/env python3
 """
-web cache and tracker
+create a web cach
 """
-import requests
 import redis
-import time
-from functools import wraps
+import requests
+rc = redis.Redis()
+count = 0
 
-# Redis connection
-redis_client = redis.Redis()
 
-def cache_result(expiration):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(url):
-            cache_key = f"count:{url}"
-            result = redis_client.get(cache_key)
-            if result is not None:
-                return result.decode("utf-8")
+def get_page(url: str) -> str:
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
-            content = func(url)
-            redis_client.setex(cache_key, expiration, content)
-            return content
-
-        return wrapper
-
-    return decorator
-
-@cache_result(expiration=10)
-def get_page(url):
-    response = requests.get(url)
-    return response.text
 
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://example.com"
-    content = get_page(url)
-    print(content)
-
+    get_page('http://slowwly.robertomurray.co.uk')
